e474eb9cc11cfb8f3312cd46ad30b89029c53bb2
Fabrizio Gutierrez <gutierrezfabrizio03@gmail.com>
2022-06-16 22:14:14 -0430

    eliminar bootstrap

--- to_do_list.php
+++ to_do_list.php
@@ -36,8 +36,6 @@ if(isset($_POST["agregar"])){
     <link rel="icon" href="img/icon-to-do.png" />
     <link rel="stylesheet" href="css/main.css" />
     <title>To Do List</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body class="body">
@@ -69,7 +67,7 @@ if(isset($_POST["agregar"])){
     </header>
     <main>
         <div class="encabezado">
-            <form action="" method="POST">
+            <form action="" method="POST" class="form-todo">
                  <div class="card-Container">
                     <label for="title">
                         <h1 class="card-Title">Crear evento</h1>
